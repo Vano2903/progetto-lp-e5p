cdb5bbffcf121caa0d2f97ff2e3677276b33073f
vano2903 <davidevanoncini2003@gmail.com>
2024-08-31 19:48:56 +0200

    initial commit with project structure

---
+++ Lisp/intar.lisp
@@ -0,0 +1,8 @@
+;; -*- Mode: Lisp -*-
+
+;; Gianfarelli	Giorgio	
+;; Lauria	Luca	
+;; Vanoncini	Davide	903214
+
+;; Interval Arithmetic
+;; This file contains the implementation of the interval arithmetic operations
