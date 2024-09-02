fc052ae9de72f2acc3fdce8928dbbaef146827ba
Luca Lauria <62106858+NERAFF@users.noreply.github.com>
2024-09-02 22:15:12 +0200

    Interval costruction

--- Prolog/intar.pl
+++ Prolog/intar.pl
@@ -5,6 +5,8 @@
 % Vanoncini	Davide	903214
 
 % definizione di reale esteso.
+
+% extended_real(infinity).
 extended_real(pos_infinity).
 extended_real(neg_infinity).
 extended_real(X) :- 
@@ -196,12 +198,52 @@ div_e(_, _, _) :-
 the interval arithmetic operations.
 */
 
-empty_interval([])
+empty_interval([]).
 % This predicate is true only of the empty interval [].
 
-interval([])
-interval(X, SI)
-interval(L, H, I)
+interval([]).
+
+interval(neg_infinity, SI) :- 
+    extended_real(neg_infinity),
+    SI = [neg_infinity, neg_infinity],
+    !.
+interval(pos_infinity, SI) :- 
+    extended_real(pos_infinity),
+    SI = [pos_infinity, pos_infinity],
+    !.
+interval(X, SI) :- 
+    extended_real(X),
+    SI = [X, X],
+    !.
+interval(neg_infinity, H, I) :-
+    extended_real(neg_infinity),
+    extended_real(H),
+    I = [neg_infinity, H],
+    !.
+interval(L, pos_infinity, I) :-
+    extended_real(L),
+    extended_real(pos_infinity),
+    I = [L, pos_infinity],
+    !.
+interval(L, neg_infinity, I) :-
+    extended_real(L),
+    extended_real(neg_infinity),
+    I = [],
+    !,
+    fail.
+interval(pos_infinity, H, I) :-
+    extended_real(pos_infinity),
+    extended_real(H),
+    I = [].
+    !,
+    fail.
+interval(L, H, I) :-
+    extended_real(L),
+    extended_real(H),
+    L =< H,
+    I = [L, H].
+interval(L, H, I) :-
+    I = [].
 /* The predicate interval/1 serves to construct an empty interval.
 The predicate interval/2 constructs a singleton interval SI containing only X. X must be
 instantiated and be an extended real, otherwise the predicate fails.
@@ -210,42 +252,64 @@ point. L and H must be instantiated and be extended reals, otherwise the predica
 that I can be the empty interval if L > H.
 */
 
-is_interval(I)
+is_interval([]).
+is_interval([pos_infinity, neg_infinity]) :-
+    !. 
+is_interval([L, H]) :- 
+    extended_real(L),
+    extended_real(H),
+    L =< H,
+    !.
+ 
 /* The predicate is interval/1 is true if I is a term representing an interval (including the empty
 interval).
 */
 
-whole_interval(R)
+whole_interval([neg_infinity, pos_infinity]).
+
 % The predicate whole interval/1 is true if R is a term representing the whole interval R.
 
-is_singleton(S)
+is_singleton([X, X]) :-
+    extended_real(X).
 % The predicate is singleton/1 is true if S is a term representing a singleton interval.
 
-iinf(I, L)
+iinf([L, _], L).
 % The predicate iinf/2 is true if I is a non empty interval and L is its inferior limit.
 
-isup(I, H)
+isup([_, H], H).
 % The predicate isup/2 is true if I is a non empty interval and H is its superior limit.
 
-icontains(I, C)
+icontains([L1, H1], C) :-
+    ( number(C) ->
+        C >= L1,
+        C =< H1
+    ; is_interval(C) ->
+        is_interval(C),
+        iinf(C, L2),
+        isup(C, H2),
+        L2 >= L1,
+    H2 =< H1
+).
 /* If I is not an interval, or if it is an empty interval, the predicate fails. Otherwise, given the
 interval I it will succeed if I contains X. X can be a number or another interval.
 */
 
-ioverlap(I1, I2)
+ioverlap([L1, H1], [L2, H2]) :-
+    H1 >= L2,
+    H2 >= L1.
 /* The predicate ioverlaps succeeds if the two intervals I1 and I2 “overlap”. The predicate fails
 if either I1 or I2 is not an interval.
 */
-
+%end of interval construction
 
 
 /* Interval Arithmetic Predicates. The following predicates implement the interval arithmetic
 operations.
 */
 
-iplus(ZI)
-iplus(X, R)
-iplus(X, Y, R)
+iplus(ZI).
+iplus(X, R).
+iplus(X, Y, R).
 /* The predicate iplus/1 is true if ZI is a non empty interval.
 The predicate iplus/2 is true if X is an instantiated non empty interval and R unifies with it,
 or if X is an instantiated extended real and R is a singleton interval containing only X.
@@ -255,8 +319,8 @@ or Y are instantiated extended reals, they are first transformed into singleton 
 In all other cases the predicates fail.
 */
 
-iminus(X, R)
-iminus(X, Y, R)
+iminus(X, R).
+iminus(X, Y, R).
 /* The predicate iminus/2 is true if X is an instantiated non empty interval and R unifies with
 its reciprocal with respect to the summation operation. If X is an extended real then it is first
 transformed into a singleton interval.
@@ -266,9 +330,9 @@ or Y are instantiated extended reals, they are first transformed into singleton 
 In all other cases the predicates fail.
 */
 
-itimes(ZI)
-itimes(X, R)
-itimes(X, Y, R)
+itimes(ZI).
+itimes(X, R).
+itimes(X, Y, R).
 /* The predicate itimes/1 is true if ZI is a non empty interval.
 The predicate itimes/2 is true if X is an instantiated non empty interval and R unifies with it,
 or if X is an instantiated extended real and R is a singleton interval containing only X.
@@ -278,10 +342,10 @@ X or Y are instantiated extended reals, they are first transformed into singleto
 In all other cases the predicates fail.
 */
 
-idiv(X, R)
-idiv(X, Y, R)
+idiv(X, R). %summation?
+idiv(X, Y, R).
 /* The predicate idiv/2 is true if X is an instantiated non empty interval and R unifies with its
-reciprocal with respect to the summation operation. If X is an extended real then it is first
+reciprocal with respect to the ''''''summation''''''' operation. If X is an extended real then it is first
 transformed into a singleton interval.
 The predicate idiv/3 is true if X and Y are instantiated non empty intervals and R is the
 interval constructed according to the division table for two non empty intervals. If either X or
